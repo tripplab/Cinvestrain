3d003b6258f6edae7b76ccb2aa5da10441af5ba7
tripplab <trippm@gmail.com>
2016-06-08 11:28:00 -0500

    Update mostrar_archivos.php

--- user/Cat/Catalogos_Ajax/subida_y_precarga_ajax-master/mostrar_archivos.php
+++ user/Cat/Catalogos_Ajax/subida_y_precarga_ajax-master/mostrar_archivos.php
@@ -18,25 +18,37 @@ $sql1="SELECT lab.id_lab FROM user,lab WHERE user.Name ='$user' AND user.lab=lab
 			}
              
 
-$sql="SELECT Id_Protocol from protocol where lab='$consul2' ";		
+$typo= $_SESSION["newsession"];
+$sql="SELECT Description from protocol where lab='$consul2' ";		
 		$cs=mysql_query($sql,$cn);
                 
 		while($resul=mysql_fetch_array($cs)){
 			$consul1=$resul[0];
 			
+                        
 			
 			}
          
                         
                         
- $directorio_escaneado = scandir('archivos_subidos'.$consul2);
+ $directorio_escaneado = scandir("archivos_subidos0");
 $archivos = array();
+
 
 foreach ($directorio_escaneado as $item) {
     if ($item != '.' and $item != '..') {
-        $archivos[] = $item;
+        
+         $quitaExtencion= explode(".", $item);
+         $quitaExtencion1=$quitaExtencion[0];
+         
+        $consulta="SELECT Description from protocol where Description='$quitaExtencion1' and lab='$consul2' and Type='$typo'";		
+         $resultado=mysql_query($consulta,$cn);
+        $totalFilas= mysql_num_rows($resultado);  
+          if($totalFilas != 0  ){
+               $archivos[] = $item;
+          }
+       
     }
 }
 echo json_encode($archivos);
 ?>
- 
